64d45e3a5aeb81dbe8b227e1456a2959b3867d28
David Melendez <davidmelendez@knights.ucf.edu>
2020-06-22 18:10:55 -0400

    Convert video into matrix

---
+++ david/test_video.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+import skvideo as vid
+import skvideo.io
+import skvideo.utils
+
+# Load video
+video = vid.utils.rgb2gray(vid.io.vread('school_smol.mp4'))
+video_shape = video.shape
+num_frames = video_shape[0]
+print('video loaded %s' % (video.shape,))
+
+video_flattened = video.reshape(-1, num_frames)
+print('video flattened %s' % (video_flattened.shape,))
+plt.imshow(video_flattened)
+plt.axis('off')
+plt.show()
+
