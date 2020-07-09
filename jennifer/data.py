b36b8477aa6eacfa7eec241544786b916a7c7697
jenniferzyy <jenniferzyy1012@gmail.com>
2020-07-09 16:13:38 -0500

    plot annotations

--- jennifer/data.py
+++ jennifer/data.py
@@ -15,15 +15,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # import dataset
-covid = pd.read_csv("covid_state.csv")
+#covid = pd.read_csv("../data/covid data/covid_state.csv")
+covid = pd.read_csv("../data/covid data/covid_state_new.csv")
 
 # clean data
-state = covid['State']
+state_name = covid['State']
 covid_num = covid.drop(columns=["Unnamed: 0","State"])
 
 # scale each state
-covid_scaled = (covid_num - covid_num.mean())/covid_num.std()
-covid_scaled.dropna(inplace=True)
+covid_scaled = (covid_num - covid_num.mean())/(covid_num.std())
+covid_scaled.dropna(inplace=True,axis='columns')
 df_covid = covid_scaled.loc[:,'2/6/20':].values # select data after 2/6/20
 
 # SVD
@@ -39,7 +40,7 @@ plt.savefig('svd_scree_plot.png',dpi=100)
 
 # data frame containing the first two singular vectors
 labels= ['SV'+str(i) for i in range(1,3)]
-svd_df = pd.DataFrame(u[:,0:2], index=state.tolist(), columns=labels)
+svd_df = pd.DataFrame(u[:,0:2], index=state_name.tolist(), columns=labels)
 svd_df=svd_df.reset_index()
 svd_df.rename(columns={'index':'State'}, inplace=True)
 svd_df.head()
@@ -52,12 +53,15 @@ svd_df['party'] = state['party']
 sns.scatterplot(x="SV1", y="SV2", hue="party", 
                 data=svd_df, s=100,
                 alpha=0.7)
+for j in range(svd_df.shape[0]):  
+    plt.text(svd_df['SV1'][j],svd_df['SV2'][j],svd_df['State'][j])
 plt.xlabel('SV 1: {0}%'.format(var_explained[0]*100), fontsize=16)
 plt.ylabel('SV 2: {0}%'.format(var_explained[1]*100), fontsize=16)
+plt.text
 
 # data frame containing the first three singular values
 labels1= ['SV'+str(i) for i in range(1,4)]
-svd_df1 = pd.DataFrame(u[:,0:3], index=state.tolist(), columns=labels1)
+svd_df1 = pd.DataFrame(u[:,0:3], index=state_name.tolist(), columns=labels1)
 svd_df1=svd_df1.reset_index()
 svd_df1.rename(columns={'index':'State'}, inplace=True)
 svd_df1.head()
@@ -74,6 +78,7 @@ for j in range(svd_df1.shape[0]):
         ax.scatter(svd_df1['SV1'][j],svd_df1['SV2'][j],svd_df1['SV3'][j], marker = 'x', color = 'r')
     else:
         ax.scatter(svd_df1['SV1'][j],svd_df1['SV2'][j],svd_df1['SV3'][j], marker = 'o', color = 'b')    
+    ax.text(svd_df1['SV1'][j],svd_df1['SV2'][j],svd_df1['SV3'][j],svd_df1['State'][j])
 ax.view_init(25,20)
 ax.legend
 plt.show()
