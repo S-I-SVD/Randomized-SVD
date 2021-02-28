eb39868c83d900ca868034800e1fee6274aab772
katiekeegan <66965763+katiekeegan@users.noreply.github.com>
2021-02-28 18:38:10 -0500

    low rank compression updates
    fixed issue with jain and jainmod images, created low rank example images, computed low rank plots

--- katie/SVD_Code_NEW/Watermarking_New/image_tools_copy.py
+++ katie/SVD_Code_NEW/Watermarking_New/image_tools_copy.py
@@ -30,10 +30,11 @@ def compress_image(img, ratio=None, rank=None, min_energy=None, mode='determinis
 Embed a watermark in an image using the Liu & Tan algorithm
 '''
 def embed_watermark(img, watermark, scale=1):
-    
+
     img_type = img.dtype
     img = img.astype(np.float64)
     watermark = watermark.astype(np.float64)
+
         
     # Stack color channels
     img_rows, img_columns = img.shape[:2] 
@@ -50,13 +51,13 @@ def embed_watermark(img, watermark, scale=1):
         img_watermarked.shape = img_watermarked.shape[:2]
 
     # Handle overflow/underflow issues
-    
+    '''
     if np.issubdtype(img_type, np.integer):
         img_watermarked = np.clip(img_watermarked, 0, 255)
     else:
         img_watermarked = np.clip(img_watermarked, 0, 1)
-
-    img_watermarked = img_watermarked.astype(img_type)
+    '''
+    #img_watermarked = img_watermarked.astype(img_type)
     return img_watermarked, watermarked_u, mat_s, watermarked_vh
 '''
 Extract a watermark from an image using the Liu & Tan algorithm
@@ -93,16 +94,16 @@ def extract_watermark(img_watermarked, watermarked_u, mat_s, watermarked_vh, sca
     # Get rid of redundant dimensions
     if watermark.shape[2] == 1:
         watermark.shape = watermark.shape[:2]
-
+    '''
     # Handle overflow/underflow issues
     
     if np.issubdtype(img_type, np.integer):
         watermark = np.clip(watermark, 0, 255)
     else:
         watermark = np.clip(watermark, 0, 1)
-   
+    '''
 
-    watermark = watermark.astype(img_type)
+    #watermark = watermark.astype(img_type)
     if size is None:
         return watermark
     else:
@@ -137,14 +138,16 @@ def embed_watermark_jain(img, watermark, scale=1, term=False):
     # Get rid of redundant dimensions
     if img_watermarked.shape[2] == 1:
         img_watermarked.shape = img_watermarked.shape[:2]
-
+    '''
     # Handle overflow/underflow issues
     if np.issubdtype(img_type, np.integer):
         img_watermarked = np.clip(img_watermarked, 0, 255)
     else:
         img_watermarked = np.clip(img_watermarked, 0, 1)
-    img_watermarked = img_watermarked.astype(img_type)
-    watermark_vh = watermark_vh.astype(watermark_type)
+    '''
+    #img_watermarked = img_watermarked.astype(img_type)
+    #watermark_vh = watermark_vh.astype(watermark_type)
+    
     if term:
         return img_watermarked, watermark_vh, jain_term
     else:
@@ -156,17 +159,17 @@ Extract a watermark from an image using the Jain algorithm
 def extract_watermark_jain(img_watermarked, img_original, watermark_vh, scale, size=None):
     watermark_type = watermark_vh.dtype
     img_type = img_watermarked.dtype
+    img_watermarked = img_watermarked.astype(np.float64)
+    img_original = img_original.astype(np.float64)
         
     # Stack color channels
     img_rows, img_columns = img_watermarked.shape[:2] 
-    img_watermarked = img_watermarked.astype(np.float64)
     img_watermarked_stacked = img_watermarked.reshape(img_rows, -1)
 
     orig_rows, orig_columns = img_original.shape[:2] 
-    img_original = img_original.astype(np.float64)
     img_original_stacked = img_original.reshape(orig_rows, -1)
 
-    #watermark_vh = watermark_vh.astype(np.float64)
+    watermark_vh = watermark_vh.astype(np.float64)
     
     # Extract watermark
     watermark_stacked = wm.extract_watermark_jain(img_watermarked_stacked, img_original_stacked, watermark_vh, scale=scale)
@@ -175,14 +178,14 @@ def extract_watermark_jain(img_watermarked, img_original, watermark_vh, scale, s
     # Get rid of redundant dimensions
     if watermark.shape[2] == 1:
         watermark.shape = watermark.shape[:2]
-
+    '''
     # Handle overflow/underflow issues
     if np.issubdtype(watermark_type, np.integer):
         watermark = np.clip(watermark, 0, 255)
     else:
         watermark = np.clip(watermark, 0, 1)
-
-    watermark = watermark.astype(watermark_type)
+    '''
+    #watermark = watermark.astype(watermark_type)
 
     if size == None:
         return watermark
@@ -216,22 +219,20 @@ def embed_watermark_jain_mod(img, watermark, scale=1, term=False):
     # Get rid of redundant dimensions
     if img_watermarked.shape[2] == 1:
         img_watermarked.shape = img_watermarked.shape[:2]
-
+    '''
     # Handle overflow/underflow issues
 
     if np.issubdtype(img_type, np.integer):
         img_watermarked = np.clip(img_watermarked, 0, 255)
     else:
         img_watermarked = np.clip(img_watermarked, 0, 1)
-
-    img_watermarked = img_watermarked.astype(img_type)
-    watermark_vh = watermark_vh.astype(watermark_type)
+    '''
+    #img_watermarked = img_watermarked.astype(img_type)
+    #watermark_vh = watermark_vh.astype(watermark_type)
     if term:
         return img_watermarked, watermark_vh, jain_mod_term
     else:
         return img_watermarked, watermark_vh
-
-
 '''
 Extract a watermark from an image using the Jain algorithm
 '''
@@ -257,15 +258,15 @@ def extract_watermark_jain_mod(img_watermarked, img_original, watermark_vh, scal
     # Get rid of redundant dimensions
     if watermark.shape[2] == 1:
         watermark.shape = watermark.shape[:2]
-
+    '''
     # Handle overflow/underflow issues
 
     if np.issubdtype(watermark_type, np.integer):
         watermark = np.clip(watermark, 0, 255)
     else:
         watermark = np.clip(watermark, 0, 1)
-
-    watermark = watermark.astype(watermark_type)
+    '''
+    #watermark = watermark.astype(watermark_type)
 
     if size == None:
         return watermark
@@ -273,8 +274,6 @@ def extract_watermark_jain_mod(img_watermarked, img_original, watermark_vh, scal
         return watermark[:size[0], :size[1]]
     
 #additional watermarking tools
-        
-
 
 '''
 Pad an image in the first two dimensions (width x height)
@@ -288,3 +287,40 @@ def load_image(path, dtype=np.uint8):
 
 def save_image(img, path):
     Image.fromarray(img.clip(0,255).astype(np.uint8)).convert('RGB').save(path)
+    
+'''
+Low rank approx (used in watermark_experiments)
+'''
+def lowrankapprox(img, k):
+    #formatting
+    img_type = img.dtype
+    img = img.astype(np.float64)
+    
+    #stacking color channels
+    img_rows, img_columns = img.shape[:2] 
+    img_stacked = img.reshape(img_rows, -1)
+    
+    #computing svd
+    U, S, VT = np.linalg.svd(img_stacked, full_matrices=False)
+    S = np.diag(S)
+    
+    #construct approximate image from U, S, VT with rank k
+    img_approx_stacked = U[:,:k] @ S[0:k,:k] @ VT[:k,:]
+    
+    #reshaping
+    img_approx = img_approx_stacked.reshape(img_rows, img_columns, -1)
+    
+    #get rid of redundant dimensions
+    if img.shape[2]==1:
+        img_approx.shape = img_approx.shape[:2]
+    '''
+    #handle overflow/underflow issues
+    if np.issubdtype(img_type, np.integer):
+        img_approx = np.clip(img_approx, 0, 255)
+    else:
+        img_approx = np.clip(img_approx, 0, 1)
+    '''    
+    #type
+    #img_approx = img_approx.astype(img_type)
+    #retun approximated image
+    return img_approx
