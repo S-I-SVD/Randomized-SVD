4791b30967a0f0332274917450c1a81e35797481
katiekeegan <66965763+katiekeegan@users.noreply.github.com>
2022-06-11 21:30:47 -0400

    Update svd_tools_copy.py

--- katie/SVD_Code_NEW/Watermarking_New/svd_tools_copy.py
+++ katie/SVD_Code_NEW/Watermarking_New/svd_tools_copy.py
@@ -434,9 +434,8 @@ def modify_sigmas_log_s_rank_truncation(img, scalar, s, rank):
     length_S = len(S)
     for i in range(length_S):
         S[i] = (math.log10(S[i]+1))**scalar
-    S = np.diag(S)
     
-    img_modified_1 = U[:, :rank] @ S[:rank] @ VT[:rank, :]
+    img_modified_1 = U[:, :rank] @ np.diag(S[:rank]) @ VT[:rank, :]
     img_modified_1 = img_modified_1.reshape(img_rows,img_columns,-1)
     
     #identifying scaling values based on logarithmic mapping
